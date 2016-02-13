283e782c3f7f6f651d4e48449f355adf64cdf67a
cazakas <danielrumayo@hotmail.com>
2016-02-13 17:32:19 +0100

    Falta Arreglar getTerm y getTermPostings

--- P1/src/es/uam/eps/bmi/search/indexing/LuceneIndex.java
+++ P1/src/es/uam/eps/bmi/search/indexing/LuceneIndex.java
@@ -34,95 +34,88 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
+import org.apache.lucene.document.Fieldable;
 import org.apache.lucene.index.IndexReader;
-
-
-
 
 /**
  *
  * @author dani
  */
-public class LuceneIndex implements Index{
+public class LuceneIndex implements Index {
+
     String indexPath;
     private IndexReader reader = null;
-    private static long num_id=0;
-    
-/* 
-    dos argumentos de entrada: la ruta de la carpeta que contiene la colección de documentos con
-los que crear el índice, y la ruta de la carpeta en la que almacenar el índice creado.*/
+    private static long num_id = 0;
 
+    /* 
+     dos argumentos de entrada: la ruta de la carpeta que contiene la colección de documentos con
+     los que crear el índice, y la ruta de la carpeta en la que almacenar el índice creado.*/
     /**
      *
      * @param args
-     */  
-    public static void main(String[] args){
-	if (args.length != 3){
-		System.out.println("Error en número de argumentos");
-	}
+     */
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("Error en número de argumentos");
+        }
         String inputCollectionPath = "src/es/uam/eps/bmi/clueweb-1K";
-	String outputCollectionPath = "outputCollection";
-       	LuceneIndex LucIdx = new LuceneIndex(inputCollectionPath,outputCollectionPath,new HTMLSimpleParser()); 
-     
+        String outputCollectionPath = "outputCollection";
+        LuceneIndex LucIdx = new LuceneIndex(inputCollectionPath, outputCollectionPath, new HTMLSimpleParser());
+
     }
-    
-    public LuceneIndex (String inputCollectionPath, String outputIndexPath, TextParser textParser){
-	    this.indexPath = inputCollectionPath;
-	    
-        build(inputCollectionPath,outputIndexPath,textParser);
+
+    public LuceneIndex(String inputCollectionPath, String outputIndexPath, TextParser textParser) {
+        this.indexPath = inputCollectionPath;
+
+        build(inputCollectionPath, outputIndexPath, textParser);
     }
-    public LuceneIndex (){
-	    
-	    
-       
+
+    public LuceneIndex() {
+
     }
-    
+
     //  docDir coincide con la ruta de los zips? hay que java.util.zip.ZipInputStream?
     // writer es donde se genera el indice. En el path indicado por dir (indexpath), y con la configuracion
     // indicada por iwc?
     @Override
     public void build(String inputCollectionPath, String outputIndexPath, TextParser textParser) {
-        boolean create=true;
-        
-     
+        boolean create = true;
+
         try {
-      System.out.println("Indexing to directory '" + inputCollectionPath + "'...");
-      this.indexPath=outputIndexPath;
-      Directory dir = FSDirectory.open(new File(this.indexPath));
-      Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
-      IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_31, analyzer);
+            System.out.println("Indexing to directory '" + inputCollectionPath + "'...");
+            this.indexPath = outputIndexPath;
+            Directory dir = FSDirectory.open(new File(this.indexPath));
+            Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
+            IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_31, analyzer);
 
-      final ZipFile docDir = new ZipFile(inputCollectionPath+"/docs.zip");
-       if (docDir.entries() == null ) {
-      System.out.println("Document directory '" +docDir + "' does not exist or is not readable, please check the path");
-      System.exit(1);
-    }
-      
-      if (create) {
+            final ZipFile docDir = new ZipFile(inputCollectionPath + "/docs.zip");
+            if (docDir.entries() == null) {
+                System.out.println("Document directory '" + docDir + "' does not exist or is not readable, please check the path");
+                System.exit(1);
+            }
+
+            if (create) {
         // Create a new index in the directory, removing any
-        // previously indexed documents:
-        iwc.setOpenMode(OpenMode.CREATE);
-      } else {
-        // Add new documents to an existing index:
-        iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
-      }
-      IndexWriter writer = new IndexWriter(dir, iwc);
-      indexDocs(writer, docDir,textParser);
-      writer.close();
+                // previously indexed documents:
+                iwc.setOpenMode(OpenMode.CREATE);
+            } else {
+                // Add new documents to an existing index:
+                iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+            }
+            IndexWriter writer = new IndexWriter(dir, iwc);
+            indexDocs(writer, docDir, textParser);
+            writer.close();
 
+        } catch (IOException e) {
+            System.out.println(" caught a " + e.getClass()
+                    + "\n with message: " + e.getMessage());
+        }
 
-
-    } catch (IOException e) {
-      System.out.println(" caught a " + e.getClass() +
-       "\n with message: " + e.getMessage());
-    }
-    
-        
     }
 
     @Override
     public void load(String indexPath) {
-        this.indexPath=indexPath;
+        this.indexPath = indexPath;
         try {
             reader = IndexReader.open(FSDirectory.open(new File(indexPath)));
         } catch (IOException ex) {
@@ -134,12 +127,12 @@ los que crear el índice, y la ruta de la carpeta en la que almacenar el índice
     @Override
     public String getPath() {
         return this.indexPath;
-        
+
     }
 
     @Override
     public List<String> getDocIds() {
-        List<String> ids =new ArrayList<>();
+        List<String> ids = new ArrayList<>();
         IndexReader _reader = null;
         Document doc = null;
         try {
@@ -147,21 +140,23 @@ los que crear el índice, y la ruta de la carpeta en la que almacenar el índice
         } catch (IOException ex) {
             Logger.getLogger(LuceneIndex.class.getName()).log(Level.SEVERE, null, ex);
         }
-	if (_reader == null)
-		return null;
-        for(int i=1; i<_reader.maxDoc();i++){
+        if (_reader == null) {
+            return null;
+        }
+        for (int i = 1; i < _reader.maxDoc(); i++) {
             try {
-                doc= _reader.document(i);
+                doc = _reader.document(i);
             } catch (IOException ex) {
                 Logger.getLogger(LuceneIndex.class.getName()).log(Level.SEVERE, null, ex);
             }
-           if (doc == null)
-		   continue;
+            if (doc == null) {
+                continue;
+            }
             ids.add(doc.getFieldable("id").stringValue());
         }
-      
+
         return ids;
-         
+
     }
 
     // hay que coger todos los ids de los documentos del indice, como? busqueda de ids en 
@@ -172,53 +167,56 @@ los que crear el índice, y la ruta de la carpeta en la que almacenar el índice
         if (reader == null) {
             return null;
         }
-        TextDocument textdoc ;
+        TextDocument textdoc;
         Document doc = null;
         String name;
         String id;
-        
-        for(int i=1; i<reader.maxDoc();i++){
+
+        for (int i = 1; i < reader.maxDoc(); i++) {
             try {
-                doc= reader.document(i);
+                doc = reader.document(i);
             } catch (IOException ex) {
                 Logger.getLogger(LuceneIndex.class.getName()).log(Level.SEVERE, null, ex);
             }
-           if (doc == null)
-		   continue;
+            if (doc == null) {
+                continue;
+            }
             //si coinciden los id contruimos el TextDocument
-            if(doc.getFieldable("id").stringValue().equals(docId)){
+            if (doc.getFieldable("id").stringValue().equals(docId)) {
                 name = doc.getFieldable("name").stringValue();
                 id = doc.getFieldable("id").stringValue();
-                textdoc = new TextDocument(id,name);
+                textdoc = new TextDocument(id, name);
                 return textdoc;
-            
+
             }
         }
         return null;
-    
-    
+
     }
 
     @Override
     public List<String> getTerms() {
-        List<String> terms =new ArrayList<>();
+        List<String> terms = new ArrayList<>();
         if (reader == null) {
             return null;
         }
         Document doc = null;
-        
-        for(int i=1; i<reader.maxDoc();i++){
+
+        for (int i = 1; i < reader.maxDoc(); i++) {
             try {
-                doc= reader.document(i);
+                doc = reader.document(i);
             } catch (IOException ex) {
                 Logger.getLogger(LuceneIndex.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-           if (doc == null)
-		   continue;
-            terms.add(doc.getFieldable("name").stringValue());
+
+            if (doc == null) {
+                continue;
+            }
+            List<Fieldable> fields = doc.getFields();
+                   
+            terms.add(doc.getFieldable("contents").stringValue());
         }
-      
+
         return terms;
     }
 
@@ -227,95 +225,89 @@ los que crear el índice, y la ruta de la carpeta en la que almacenar el índice
         if (reader == null) {
             return null;
         }
-        List<Long> pos= new ArrayList<>();
-        List<Posting> posts=new ArrayList<>();
+        List<Long> pos = new ArrayList<>();
+        List<Posting> posts = new ArrayList<>();
         Document doc;
-        
-        for(int i=1; i<reader.maxDoc();i++){
+
+        for (int i = 1; i < reader.maxDoc(); i++) {
             Posting post;
             try {
-                doc= reader.document(i);
-                Term ter=new Term(term,term);
+                doc = reader.document(i);
+                Term ter = new Term(term, term);
                 Integer aaux = reader.termPositions(ter).nextPosition();
                 Long aux = aaux.longValue();
                 pos.add(aux);
-                post=new Posting(doc.getFieldable("name").stringValue(),
-                    term, pos );
+                post = new Posting(doc.getFieldable("contents").stringValue(),
+                        term, pos);
                 posts.add(post);
-                
-                
+
             } catch (IOException ex) {
                 Logger.getLogger(LuceneIndex.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
-                    
+
         }
-        
+
         return posts;
     }
-    
+
     /*Así vemos si se ha hecho el load o no*/
-     public IndexReader getReader() {
+    public IndexReader getReader() {
         return this.reader;
     }
-    
-    
+
     static void indexDocs(IndexWriter writer, ZipFile file, TextParser textParser)
-    throws IOException {
-    // do not try to index files that cannot be read
-       try {
+            throws IOException {
+        // do not try to index files that cannot be read
+        try {
 
           // make a new, empty document
+            ZipFile zipFile = file;
+            Enumeration<? extends ZipEntry> entries = zipFile.entries();
+            while (entries.hasMoreElements()) {
+                Document doc = new Document();
+                ZipEntry entry = entries.nextElement();
 
-	  ZipFile zipFile = file ;
-	  Enumeration<? extends ZipEntry> entries = zipFile.entries();
-	  while(entries.hasMoreElements()){
-          	Document doc = new Document();
-		ZipEntry entry = entries.nextElement();
-		  
-          	Field pathField = new Field("name", entry.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
-          	pathField.setIndexOptions(IndexOptions.DOCS_ONLY);
-          	doc.add(pathField);
+                Field pathField = new Field("name", entry.getName(), Field.Store.YES, Field.Index.NOT_ANALYZED_NO_NORMS);
+                pathField.setIndexOptions(IndexOptions.DOCS_ONLY);
+                doc.add(pathField);
 
-          	NumericField idField = new NumericField("id", Field.Store.YES, true);
-          	idField.setLongValue(num_id);
-          	num_id++;
-          	doc.add(idField);
-          
-          	NumericField modifiedField = new NumericField("modified");
-          	modifiedField.setLongValue(entry.getTime());
-          	doc.add(modifiedField);
-	
-		
-		InputStream stream = zipFile.getInputStream(entry);
-		BufferedReader br = new BufferedReader(new InputStreamReader(stream));
-		
-		String sCurrentLine;
-		String content = "";
-		
-		while ((sCurrentLine = br.readLine()) != null) {
-				content = content + sCurrentLine;
-			}
+                NumericField idField = new NumericField("id", Field.Store.YES, true);
+                idField.setLongValue(num_id);
+                num_id++;
+                doc.add(idField);
 
-		String toad = textParser.parse(content);
-		doc.add(new Field("contents", toad ,Field.Store.NO, Field.Index.ANALYZED_NO_NORMS));
-		
+                NumericField modifiedField = new NumericField("modified");
+                modifiedField.setLongValue(entry.getTime());
+                doc.add(modifiedField);
 
-          if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
-            // New index, so we just add the document (no old document can be there):
-            System.out.println("adding " + entry.getName());
-            writer.addDocument(doc);
-          } else {
+                InputStream stream = zipFile.getInputStream(entry);
+                BufferedReader br = new BufferedReader(new InputStreamReader(stream));
+
+                String sCurrentLine;
+                String content = "";
+
+                while ((sCurrentLine = br.readLine()) != null) {
+                    content = content + sCurrentLine;
+                }
+
+                String toad = textParser.parse(content);
+                doc.add(new Field("contents", toad, Field.Store.YES, Field.Index.ANALYZED_NO_NORMS));
+
+                if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
+                    // New index, so we just add the document (no old document can be there):
+                    System.out.println("adding " + entry.getName());
+                    writer.addDocument(doc);
+                } else {
             // Existing index (an old copy of this document may have been indexed) so 
-            // we use updateDocument instead to replace the old one matching the exact 
-            // path, if present:
-            System.out.println("updating " + entry.getName());
-            writer.updateDocument(new Term("name", entry.getName()), doc);
-          }
-	  }
-          
+                    // we use updateDocument instead to replace the old one matching the exact 
+                    // path, if present:
+                    System.out.println("updating " + entry.getName());
+                    writer.updateDocument(new Term("name", entry.getName()), doc);
+                }
+            }
+
         } finally {
-	   file.close();
+            file.close();
         }
-      }
     }
+}
