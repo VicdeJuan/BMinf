12e25ee1c4f66a3e6c94938c746a9dd7038a7cf7
VicdeJuan <dejuan.victor@gmail.com>
2016-02-15 23:56:46 +0100

    Verguenza me da el código que acabo de escribir, pero me quiero dormir y así lo dejo ejecutando

--- P1/src/es/uam/eps/bmi/search/indexing/TestIndex.java
+++ P1/src/es/uam/eps/bmi/search/indexing/TestIndex.java
@@ -9,18 +9,14 @@ import es.uam.eps.bmi.search.parsing.HTMLSimpleParser;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Stream;
 
 
 /**
@@ -34,39 +30,42 @@ public class TestIndex {
     public TestIndex() {
     }
     public static void main (String[] args){
-	 String inputCollectionPath = "src/es/uam/eps/bmi/clueweb-1K";
-        String outputCollectionPath = "outputCollection";
-	boolean regen = true;
+	 String inputCollectionPath_1 = "src/es/uam/eps/bmi/clueweb-1K";
+         String inputCollectionPath_10 = "src/es/uam/eps/bmi/clueweb-10K";
+        String outputCollectionPath_1 = "outputCollection_1";
+        String outputCollectionPath_10 = "outputCollection_10";
 	
-        LuceneIndex LucIdx = regen ?  new LuceneIndex(inputCollectionPath, outputCollectionPath, new HTMLSimpleParser()) : new LuceneIndex(outputCollectionPath) ;
+        boolean regen = true;
+	
+        LuceneIndex LucIdx_1 = regen ?  new LuceneIndex(inputCollectionPath_1, outputCollectionPath_1, new HTMLSimpleParser()) : new LuceneIndex(outputCollectionPath_1) ;
+        LuceneIndex LucIdx_10 = regen ?  new LuceneIndex(inputCollectionPath_10, outputCollectionPath_10, new HTMLSimpleParser()) : new LuceneIndex(outputCollectionPath_10) ;
         
 	FileWriter fichero = null;
         try {
             fichero = new FileWriter("src/es/uam/eps/bmi/frecuencias1K.txt");
+            fichero = new FileWriter("src/es/uam/eps/bmi/frecuencias10K.txt");
         } catch (IOException ex) {
             Logger.getLogger(TestIndex.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
-        final PrintWriter pw = new PrintWriter(fichero);;
+        final PrintWriter pw_1 = new PrintWriter(fichero);    
+        final PrintWriter pw_10 = new PrintWriter(fichero);
         try {
-            
-
             // term -> (freq, ndoc, tf,idf)
-	    LinkedHashMap <String,double[]> freqHash = new LinkedHashMap<>();
+	    final LinkedHashMap <String,double[]> freqHash = new LinkedHashMap<>();
             
-                LucIdx.getTerms().stream().forEach((term) -> {
+                LucIdx_1.getTerms().stream().forEach((term) -> {
                 double[] val;
                 val = new double[] {0,0,0,0};
-                List<Posting> termPostings = LucIdx.getTermPostings(term);
+                List<Posting> termPostings = LucIdx_1.getTermPostings(term);
                 termPostings.stream().forEach((post) -> {
                     val[0] += post.getTermFrequency();
                     val[1]++;
                     val[2] += post.getTermFrequency() == 0 ? 1 : 1 + Math.log(post.getTermFrequency())/Math.log(2);
                 });
                 // val 2 = tf
-                val[2] = val[2]/LucIdx.getNumDoc();
+                val[2] = val[2]/LucIdx_1.getNumDoc();
                 // val 3 = idf
-                val[3] = Math.log(LucIdx.getNumDoc()/val[1]);
+                val[3] = Math.log(LucIdx_1.getNumDoc()/val[1]);
                 freqHash.put(term,val);
              });
             
@@ -74,8 +73,37 @@ public class TestIndex {
             Collections.sort(sorted, (Entry<String, double[]> o1, Entry<String, double[]> o2) -> Double.compare(o2.getValue()[0],o1.getValue()[0]));
             
             sorted.stream().forEach((val) -> {
-                pw.write(String.format("%s %f %f %f %f\n", val.getKey(), val.getValue()[0],val.getValue()[1],val.getValue()[2],val.getValue()[3]));
+                pw_1.write(String.format("%s %f %f %f %f\n", val.getKey(), val.getValue()[0],val.getValue()[1],val.getValue()[2],val.getValue()[3]));
             });
+            
+            
+            
+            // term -> (freq, ndoc, tf,idf)
+	    final LinkedHashMap <String,double[]> freqHash_10 = new LinkedHashMap<>();
+            
+                LucIdx_10.getTerms().stream().forEach((term) -> {
+                double[] val;
+                val = new double[] {0,0,0,0};
+                            List<Posting> termPostings = LucIdx_10.getTermPostings(term);
+                termPostings.stream().forEach((post) -> {
+                    val[0] += post.getTermFrequency();
+                    val[1]++;
+                    val[2] += post.getTermFrequency() == 0 ? 1 : 1 + Math.log(post.getTermFrequency())/Math.log(2);
+                });
+                // val 2 = tf
+                val[2] = val[2]/LucIdx_10.getNumDoc();
+                // val 3 = idf
+                val[3] = Math.log(LucIdx_10.getNumDoc()/val[1]);
+                freqHash_10.put(term,val);
+             });
+            
+            sorted = new LinkedList <>(freqHash.entrySet());
+            Collections.sort(sorted, (Entry<String, double[]> o1, Entry<String, double[]> o2) -> Double.compare(o2.getValue()[0],o1.getValue()[0]));
+            
+            sorted.stream().forEach((val) -> {
+                pw_10.write(String.format("%s %f %f %f %f\n", val.getKey(), val.getValue()[0],val.getValue()[1],val.getValue()[2],val.getValue()[3]));
+            });
+           
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
--- P1/src/es/uam/eps/bmi/search/parsing/HTMLSimpleParser.java
+++ P1/src/es/uam/eps/bmi/search/parsing/HTMLSimpleParser.java
@@ -24,9 +24,9 @@ public class HTMLSimpleParser implements TextParser {
 	   // Element doc;
 	    //doc = parseDoc(text).body();
 	    //return doc.text();
-            String aux = text.split("DOCTYPE")[1];
-           String toret =  Jsoup.parse(aux).text();
-           toret = toret.replaceAll(",|\\.|'s", "");
+           //String aux = text.split("DOCTYPE")[1];
+           String toret =  Jsoup.parse(text).text();
+           //toret = toret.replaceAll(",|\\.|'s", "");
            return toret;
     }
     public org.jsoup.nodes.Document parseDoc(String text) {
