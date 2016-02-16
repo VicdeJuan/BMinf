6e6eb953832a4f13900ececdecf2c947b12d57f1
VicdeJuan <dejuan.victor@gmail.com>
2016-02-16 21:09:23 +0100

    Todo listo para generar los pdfs

--- Memoria/generarGraficas.R
+++ Memoria/generarGraficas.R
@@ -1,62 +1,61 @@
 #Si cambias la variable pch por "*", salen mas gordos los puntos que con ".", no se que  queda mejor.
 
 
-setwd("~/Escritorio/practica BMinf") #directorio donde se encuentran los txt de frecuencias
 
 
 #_____________ Frecuencias 1K ___________________
-datos = read.table("frecuencias1K.txt")
+datos = read.table("data/frecuencias1K")
 x <- 1:length(datos$V1)
 
 #frecuencia
 y <- datos$V2
-png('frecuencias1k.png')
+png('img/frecuencias1k.png')
 plot(x,log(y), pch="*", col="blue", xlab="termino", ylab="frecuencia", main="frecuencias 1k")
 dev.off()
 
 #num docs
 y <- datos$V3
-png('numdocs1k.png')
+png('img/numdocs1k.png')
 plot(x,log(y), pch=".", col="blue", xlab="termino", ylab="nº docs", main="nº docs 1k")
 dev.off()
 
 #tf
 y <- datos$V4
-png('tf1k.png')
+png('img/tf1k.png')
 plot(x,log(y), pch=".", col="blue", xlab="termino", ylab="tf", main="tf 1k")
 dev.off()
 
 #idf
 y <- datos$V5
-png('idf1k.png')
+png('img/idf1k.png')
 plot(x,log(y), pch=".", col="blue", xlab="termino", ylab="idf", main="idf 1k")
 dev.off()
 
 #_____________ Frecuencias 10K ___________________
-datos = read.table("frecuencias10K.txt")
+datos = read.table("data/frecuencias10K")
 x <- 1:length(datos$V1)
 
 #frecuencia
 y <- datos$V2
-png('frecuencias10k.png')
+png('img/frecuencias10k.png')
 plot(x,log(y), pch="*", col="blue", xlab="termino", ylab="frecuencia", main="frecuencias 10k")
 dev.off()
 
 #num docs
 y <- datos$V3
-png('numdocs10k.png')
+png('img/numdocs10k.png')
 plot(x,log(y), pch=".", col="blue", xlab="termino", ylab="nº docs", main="nº docs 10k")
 dev.off()
 
 #tf
 y <- datos$V4
-png('tf10k.png')
+png('img/tf10k.png')
 plot(x,log(y), pch=".", col="blue", xlab="termino", ylab="tf", main="tf 10k")
 dev.off()
 
 #idf
 y <- datos$V5
-png('idf10k.png')
+png('img/idf10k.png')
 plot(x,log(y), pch=".", col="blue", xlab="termino", ylab="idf", main="idf 10k")
 dev.off()
 
--- Memoria/precisiones.zsh
+++ Memoria/precisiones.zsh
@@ -1,6 +1,16 @@
 
+echo "" > tex/precisiones.tex
 for f in $(ls data/precisiones);do
 	ruby precisiones.rb "data/precisiones/$f" $f >> tex/precisiones.tex
 done
 
 pdflatex "bmi-p1-12-precisiones"
+
+Rscript generarGraficas.R
+
+echo "" > tex/frecuencias.tex
+for f in $(ls img/*.png); do
+	echo "\\easyimg{$f}{}{}{0.5}" >> tex/frecuencias.tex
+done
+
+pdflatex "bmi-p1-12-frecuencias"
