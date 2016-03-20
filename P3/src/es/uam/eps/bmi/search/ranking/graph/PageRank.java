a9b4a410f7c65675f1ef8528bca23036c58d7f08
Víctor de Juan <dejuan.victor@gmail.com>
2016-03-20 06:22:09 +0100

    PageRankTest3 contestando a lo que piden

--- P3/src/es/uam/eps/bmi/search/ranking/graph/PageRank.java
+++ P3/src/es/uam/eps/bmi/search/ranking/graph/PageRank.java
@@ -8,21 +8,24 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.OptionalDouble;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
-
+import org.apache.commons.collections4.comparators.ComparableComparator;
 
 public class PageRank {
-	
+
 	public double DEFAULT_PRECISSION = 0.00005;
 
 	public final String filetowrite = "colecciones/pageRank/page_rank1K.txt";
-	
+
 	private String fileOfLinks;
 	private Matrix matrix;
 	private double[] scores;
@@ -31,76 +34,90 @@ public class PageRank {
 	private int countDocs; // Variable para crear la matriz del grafo.
 	private double r; // r entre 0 y 1. Ponderación del peso de la probabilidad de teleportación.
 	private boolean scoresCalculated = false;
-	
+
 	private final int MAX_ITER = 100;
-	
-	public void writeValues(){
+
+	public void writeValues() {
+		int lim = 10;
+		
+		List<String> docs = dicNameDocID.entrySet().stream().sorted(
+			(
+				Map.Entry<String, Integer> o1,
+				Map.Entry<String, Integer> o2)
+			-> Double.compare(scores[o2.getValue()], scores[o1.getValue()])
+		).limit(lim).map(
+			(ent) -> ent.getKey()
+		).collect(Collectors.toList());
+
 		try {
 			FileWriter fw = new FileWriter(filetowrite);
 			BufferedWriter bw = new BufferedWriter(fw);
-			
-			if (!scoresCalculated)
+
+			if (!scoresCalculated) {
 				calculateScores();
-			
-			for (Map.Entry<String, Integer> e : dicNameDocID.entrySet()){
-				bw.write(e.getKey() + " " + scores[e.getValue()] + "\r\n");
+			}
+
+			for (String d : docs) {
+				bw.write(d + " " + scores[dicNameDocID.get(d)] + "\r\n");
+				bw.write(getFirstLines(d));
 			}
 			bw.close();
-			
+
 		} catch (IOException ex) {
 			Logger.getLogger(PageRank.class.getName()).log(Level.SEVERE, null, ex);
 		}
 	}
 
-	public PageRank(String fileOfLinks, int collectionSize,double r) {
-		
+	public PageRank(String fileOfLinks, int collectionSize, double r) {
+
 		//int nrow;
 		//nrow = 1;
 		dicNameDocID = new LinkedHashMap<>();
-		matrix = new Matrix( collectionSize, collectionSize);
+		matrix = new Matrix(collectionSize, collectionSize);
 		this.r = r;
-		load(fileOfLinks,collectionSize);
+		load(fileOfLinks, collectionSize);
 		scores = new double[collectionSize];
-		
+
 	}
-	
-	
-	private Matrix _iterate(Matrix matrix, double precission, int numIterations){
+
+	private Matrix _iterate(Matrix matrix, double precission, int numIterations) {
 		Matrix m = Matrix.power(matrix, 2);
 		Matrix sc = new Matrix(scores, m.getNumRows(), 1);
-	
-		if (numIterations <= 2)
+
+		if (numIterations <= 2) {
 			return m;
-		
+		}
+
 		OptionalDouble od = DoubleStream.of(
 			Matrix.resta(
 				m,
 				matrix).
 			getData()).max();
-		
-		if (od.isPresent()){
-			if(od.getAsDouble() < precission)
+
+		if (od.isPresent()) {
+			if (od.getAsDouble() < precission) {
 				return m;
-			else{
-				
-				return _iterate(m,precission,numIterations-1);
+			} else {
+
+				return _iterate(m, precission, numIterations - 1);
 			}
 		}
-				
-		return _iterate(m,precission,numIterations);
+
+		return _iterate(m, precission, numIterations);
 	}
-	
-	public Matrix iterate(double precission,int numIterations){
+
+	public Matrix iterate(double precission, int numIterations) {
 		return _iterate(matrix, precission, numIterations);
 
 	}
-	public Matrix iterate(double precission){
-		
-		return iterate(precission,MAX_ITER);
+
+	public Matrix iterate(double precission) {
+
+		return iterate(precission, MAX_ITER);
 	}
-	
-	public Matrix iterate(int numIterations){
-		Matrix m = Matrix.power(matrix,numIterations);
+
+	public Matrix iterate(int numIterations) {
+		Matrix m = Matrix.power(matrix, numIterations);
 		return m;
 		//return m.getRow(0);
 	}
@@ -153,7 +170,7 @@ public class PageRank {
 			matrix.normalize();
 			//Añadimos la correspondiente matriz de 1/N
 			matrix.teleport(this.r);
-			
+
 			br.close();
 		} catch (FileNotFoundException ex) {
 			Logger.getLogger(PageRank.class.getName()).log(Level.SEVERE, null, ex);
@@ -163,21 +180,26 @@ public class PageRank {
 		}
 	}
 
-	public void calculateScores(){
+	public void calculateScores() {
 		scoresCalculated = true;
 		scores = iterate(DEFAULT_PRECISSION).getRow(0);
 	}
-	public double[] getScores(){
+
+	public double[] getScores() {
 		return scores;
 	}
-	public double getScoreOf(String docId) {
-		return matrix.get((int) dicNameDocID.get(docId),0);
-	}
 
+	public double getScoreOf(String docId) {
+		return matrix.get((int) dicNameDocID.get(docId), 0);
+	}
 
 	@Override
 	public String toString() {
 		return this.matrix.toString();
+	}
+
+	private char[] getFirstLines(String d) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
 	}
 
 }
