413283da44e6b98aa60d65334864ec5124ba4eb7
VicdeJuan <dejuan.victor@gmail.com>
2016-04-13 18:27:35 +0200

    Para el lab (II)

--- P4/test/es/uam/eps/bmi/search/ranking/graph/MatrixTest.java
+++ P4/test/es/uam/eps/bmi/search/ranking/graph/MatrixTest.java
@@ -44,11 +44,29 @@ public class MatrixTest {
 
     @Test
     public void testRocchiNormalize() {
-        double[] initial = {};
+        double[] initial = {
+            18.0,   12.2,   14.0,   29.5,   40.3,
+            106,    163,    74,     54,     155,
+            36,     28,     21,     26,     42,
+            11,     14,     23,     19,     19
+        };
+        
         Matrix instance = new Matrix(initial, 4, 5);
-        double[] expected = {};
-        double[] users = {};
-        instance.RocchiNormalize(new Matrix(users, 4, 10));
+        double[] expected = {
+            3.6,    2.44,   3.5,    4.9167, 8.1,
+            21.2,   32.6,   18.5,   9.0,    31.0,
+            7.2,    5.6,    5.25,   4.33,   8.4,
+            2.2,    2.8,    5.75,   3.1667, 3.8
+        };
+        
+        double[] users = {
+            1,0,4,0,5,0,2,0,0,5,
+            0,2,0,3,0,3,5,0,1,0,
+            4,0,0,0,4,0,3,0,0,2,
+            0,2,1,5,0,0,1,3,0,3,
+            3,0,4,0,3,5,0,0,4,0
+        };
+        instance.RocchiNormalize(new Matrix(users, 5, 10));
 
         for (int i = 0; i < expected.length; i++) {
             assertEquals(expected[i], instance.getData()[i], 0.000001);
